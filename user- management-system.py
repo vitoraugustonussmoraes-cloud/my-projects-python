4a077bbdd74c1f3b51833dbccf6e5a8c0a4ad52c
Vitor Augusto <vitoraugustonussmoraes@gmail.com>
2026-08-04 00:30:40 -0300

    Update list_user

--- user- management-system.py
+++ user- management-system.py
@@ -29,28 +29,62 @@ def register_user():
     print(look_user)
 def login_user():
     username = input("Type your username: ")
-    if username in len(look_user):
+    if username in look_user:
         password = input("Tapy your password: ")
-        if password == password(look_user):
+        if password == look_user[username]["password"]:
             print(f"Welcome {username}!")
+        else:
+            print("Incorrect password")
+            return
+    else: 
+        print("Username not found")
+        return
 def list_user():
     print(look_user)
 def search_user():
-    user = input("Type the username you want to found: ")
-    if user in len(look_user):
-        look_user.remove("password")
-        print(f"{user}")
+    username = input("Type the username you want to find: ")
+    if username in look_user:
+        print(f"Username: {username}")
+        print(f"Fullname: {look_user[username]['fullname']}")
+        print(f"Age: {look_user[username]['age']}")
+        print(f"Email: {look_user[username]['email'] }") 
     else:
         print("User not found...")
 def edit_user():
-    def delete_user():
-        user = input("Type the username of user you want to delete: ")
-    if user in len(look_user):
-        sure = input(f"Are you sure about delete {user}? (Y/N)")
+    username = input("Type the username you want to edit: ")
+    if username in look_user:
+        edit = input("""What do you want to edit?: 
+        1 - Full name
+        2 - Age
+        3 - Email
+        4 - Password
+        """)
+        if edit == "1":
+            newfullname = input("Type the new full name: ")
+            look_user[username]['fullname'] = newfullname
+        elif edit == "2":
+            newage = input("Type the new age: ")
+            look_user[username]['age'] = newage
+        elif edit == "3":
+            newemail = input("Tapy your new email: ")   
+            look_user[username]['email'] = newemail
+        elif edit == "4":
+            probablypassword = input("For edit your password, type the actual password: ")
+            if probablypassword == look_user[username]['password']:
+                newpassword = input("Tapy your new password: ")
+                look_user[username]['password'] = newpassword
+            else:
+                print("Password Invalid")
+        else: 
+            print("Option is not valid")
+    else:
+        print("Press ENTER to return to the menu")
+def delete_user():
+    username = input("Type the username of user you want to delete: ")
+    if username in (look_user):
+        sure = input(f"Are you sure about delete {username}? (Y/N)")
         if sure.lower() == "y":
-            look_user.remove(user)
-        elif sure.lower() == "n":
-            print("Press ENTER to return to the menu")
+            del look_user[username]
         else:
             print("Press ENTER to return to the menu")
 while True:
